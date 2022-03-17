0184274b4a0eb59393ad6fdbaebe0ba0180f9bde
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-03-17 12:54:14 +0500

    create a file structure

---
+++ src/Components/data.js
@@ -0,0 +1,20 @@
+export const data = [
+  {
+    name: "awais",
+    rollno: 5157,
+    class: "web and mobile development",
+    batch: "batch-04",
+  },
+  {
+    name: "hamza",
+    rollno: 5158,
+    class: "web and mobile development",
+    batch: "batch-07",
+  },
+  {
+    name: "Waqas",
+    rollno: 5167,
+    class: "web and mobile development",
+    batch: "batch-07",
+  },
+];
---
+++ src/Components/students.js
@@ -0,0 +1,12 @@
+import  {useState} from react;
+import data, { data } from "./data"
+function Students() {
+    const [students, setStudents] = useState(data)
+    return(
+        <div>
+
+        </div>
+    )
+    
+};
+export default Students;
